0d8a6bac2bef2afdf54c63fe75124c160acdf65a
WojtekM113 <wojtek.mendlik@gmail.com>
2025-08-21 15:32:11 +0200

    Added Jump Function and Projectile
    - Projectile class and an actor with basic properties
    - Character can shoot projectile
    - Jump function on character
    - character can jump by pressing space

--- Source/ActionRogueLike/Private/SCharacter.cpp
+++ Source/ActionRogueLike/Private/SCharacter.cpp
@@ -6,6 +6,7 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
@@ -14,13 +15,15 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm Component"));
+	SpringArmComponent->bUsePawnControlRotation = true;
 	SpringArmComponent->SetupAttachment(RootComponent);
 	
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Component"));
-	CameraComponent->SetupAttachment(SpringArmComponent);	
+	CameraComponent->SetupAttachment(SpringArmComponent);
 
+	GetCharacterMovement()->bOrientRotationToMovement = true; 
+	bUseControllerRotationYaw = false;
 }
-
 // Called when the game starts or when spawned
 void ASCharacter::BeginPlay()
 {
@@ -37,19 +40,6 @@ void ASCharacter::BeginPlay()
 			}
 		}
 	}
-	
-}
-
-void ASCharacter::MoveForward(const FInputActionInstance& Instance)
-{
-	float FloatValue = Instance.GetValue().Get<float>();
-	AddMovementInput(GetActorForwardVector(), FloatValue);
-}
-
-void ASCharacter::Turn(const FInputActionInstance& Instance)
-{
-	float FloatValue = Instance.GetValue().Get<float>();
-	AddControllerYawInput(FloatValue);
 }
 
 // Called every frame
@@ -64,7 +54,59 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
 	EnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Triggered,this,&ASCharacter::MoveForward);
-	EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered,this, &ASCharacter::Turn);
-
+	EnhancedInputComponent->BindAction(LookUpAction, ETriggerEvent::Triggered,this, &ASCharacter::LookUp);
+	EnhancedInputComponent->BindAction(LookRightAction, ETriggerEvent::Triggered,this, &ASCharacter::LookRight);
+	EnhancedInputComponent->BindAction(MoveRightAction, ETriggerEvent::Triggered,this, &ASCharacter::MoveRight);
+	EnhancedInputComponent->BindAction(PrimaryAttackAction, ETriggerEvent::Triggered, this, &ASCharacter::PrimaryAttack);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ASCharacter::Jump);
 }
 
+void ASCharacter::MoveForward(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+	
+	// We want to move character towards camera forward vector
+	FRotator ControlRotation = GetControlRotation();
+	ControlRotation.Pitch = 0.0f;
+	ControlRotation.Roll = 0.0f;
+	
+	AddMovementInput(ControlRotation.Vector(), FloatValue);
+}
+
+void ASCharacter::MoveRight(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+
+	FRotator ControlRotation = GetControlRotation();
+	ControlRotation.Pitch = 0.0f;
+	ControlRotation.Roll = 0.0f;
+	
+	FVector RightVectorFromControlRotation = FRotationMatrix(ControlRotation).GetScaledAxis(EAxis::Y);
+	
+	AddMovementInput(RightVectorFromControlRotation, FloatValue);
+}
+
+void ASCharacter::LookUp(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+	AddControllerPitchInput(FloatValue);
+}
+
+void ASCharacter::LookRight(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+	AddControllerYawInput(FloatValue);
+}
+
+inline void ASCharacter::PrimaryAttack()
+{
+	FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
+	FTransform SpawnTM = FTransform(GetControlRotation(), HandLocation);
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	
+	GetWorld()->SpawnActor<AActor>(ProjectileClass,SpawnTM, SpawnParams);
+}
+
+ 
--- Source/ActionRogueLike/Public/SCharacter.h
+++ Source/ActionRogueLike/Public/SCharacter.h
@@ -18,6 +18,10 @@ class ACTIONROGUELIKE_API ASCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> ProjectileClass;
+
 public:
 	// Sets default values for this character's properties
 	ASCharacter();
@@ -28,18 +32,36 @@ protected:
 	TObjectPtr<UInputAction> MoveForwardAction;
 
 	UPROPERTY(EditAnywhere, Category= "Input")
-	TObjectPtr<UInputAction> TurnAction;
+	TObjectPtr<UInputAction> LookUpAction;
+
+	UPROPERTY(EditAnywhere, Category= "Input")
+	TObjectPtr<UInputAction> LookRightAction;
 	
+	UPROPERTY(EditAnywhere, Category= "Input")
+	TObjectPtr<UInputAction> MoveRightAction;
+
+	UPROPERTY(EditAnywhere, Category= "Input")
+	TObjectPtr<UInputAction> PrimaryAttackAction;
+
+	UPROPERTY(EditAnywhere, Category= "Input")
+	TObjectPtr<UInputAction> JumpAction;
 	UPROPERTY(VisibleAnywhere)
 	USpringArmComponent* SpringArmComponent;
 
 	UPROPERTY(VisibleAnywhere)
-	UCameraComponent* CameraComponent;
+	TObjectPtr<UCameraComponent> CameraComponent;
 	
 	virtual void BeginPlay() override;
 	
 	void MoveForward(const FInputActionInstance& Instance);
-	void Turn(const FInputActionInstance& Instance);
+
+	void MoveRight(const FInputActionInstance& Instance);
+	
+	void LookUp(const FInputActionInstance& Instance);
+	void LookRight(const FInputActionInstance& Instance);
+
+	void PrimaryAttack();
+	
 	UPROPERTY(EditAnywhere, Category= "Input")
 	TSoftObjectPtr<UInputMappingContext> InputMapping;
 
@@ -51,3 +73,5 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
+
+ 
